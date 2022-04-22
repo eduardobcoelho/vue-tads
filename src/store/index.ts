9f1ba6b808c3061ed3f40438ef5ce75c89a94e7b
luiz.eduardo <luiz.eduardo@usemobile.xyz>
2022-04-22 12:55:13 -0300

    feat: vuex-persist lib

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,16 @@
 import { createStore } from 'vuex';
+import VuexPersist from 'vuex-persist';
+// Módulos
 import TADModule from './TAD';
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage,
+});
 
 export default createStore({
   modules: {
     TAD: TADModule,
   },
+  plugins: [vuexLocalStorage.plugin],
 });
