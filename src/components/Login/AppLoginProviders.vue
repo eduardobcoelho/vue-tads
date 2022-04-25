40531557b5f31a9862ca2d1136ce70002ae813a9
luiz.eduardo <luiz.eduardo@usemobile.xyz>
2022-04-25 16:26:38 -0300

    feat: added component AppLoginProviders

---
+++ src/components/Login/AppLoginProviders.vue
@@ -0,0 +1,62 @@
+<template>
+  <n-button
+    v-for="{ provider, color } in authOptions"
+    :key="provider"
+    @click="signIn(provider.toLowerCase())"
+    :color="color"
+    class="providers-button"
+  >
+    <Icon size="32">
+      <template v-if="provider === 'Google'">
+        <LogoGoogle></LogoGoogle>
+      </template>
+      <template v-else-if="provider === 'Facebook'">
+        <LogoFacebook></LogoFacebook>
+      </template>
+      <template v-else>
+        <LogoGithub></LogoGithub>
+      </template>
+    </Icon>
+    <span>{{ provider }}</span>
+  </n-button>
+</template>
+
+<script lang="ts" setup>
+  import { defineEmits } from 'vue';
+  import { useStore } from 'vuex';
+  import { Icon } from '@vicons/utils';
+  import { LogoGoogle, LogoGithub, LogoFacebook } from '@vicons/carbon';
+
+  const emit = defineEmits(['setErrorStatus']);
+  const store = useStore();
+
+  const authOptions: { provider: string; color: string } = [
+    { provider: 'Google', color: '#DB4437' },
+    { provider: 'Facebook', color: '#4267B2' },
+    { provider: 'Github', color: '#171515' },
+  ];
+
+  function signIn(provider: string) {
+    emit('setErrorStatus', false);
+    store.dispatch('signIn', provider).catch(() => {
+      emit('setErrorStatus', true);
+    });
+  }
+</script>
+
+<style lang="scss" scoped>
+  .providers-button {
+    width: 100%;
+    padding: 24px 0;
+
+    &:not(&:nth-child(1)) {
+      margin-top: 14px;
+    }
+  }
+
+  span {
+    font-size: 16px;
+    font-weight: 600;
+    margin-left: 4px;
+  }
+</style>
--- src/views/AppLogin.vue
+++ src/views/AppLogin.vue
@@ -3,26 +3,13 @@
     <h1>Login</h1>
     <h4>Selecione a opção desejada</h4>
     <div class="app-login__box">
-      <n-button
-        v-for="{ provider, color } in authOptions"
-        :key="provider"
-        @click="signIn(provider.toLowerCase())"
-        :color="color"
-        class="app-login__button"
-      >
-        <Icon size="32">
-          <template v-if="provider === 'Google'">
-            <LogoGoogle></LogoGoogle>
-          </template>
-          <template v-else-if="provider === 'Facebook'">
-            <LogoFacebook></LogoFacebook>
-          </template>
-          <template v-else>
-            <LogoGithub></LogoGithub>
-          </template>
-        </Icon>
-        <span>{{ provider }}</span>
-      </n-button>
+      <AppLoginProviders
+        @setErrorStatus="
+          (value) => {
+            loginError = value;
+          }
+        "
+      ></AppLoginProviders>
       <n-space v-show="loginError" vertical style="margin-top: 14px">
         <n-alert type="error"> Tente novamente! </n-alert>
       </n-space>
@@ -32,32 +19,17 @@
 
 <script lang="ts" setup>
   import { ref } from 'vue';
-  import { useStore } from 'vuex';
   import { useRouter } from 'vue-router';
-  import { Icon } from '@vicons/utils';
-  import { LogoGoogle, LogoGithub, LogoFacebook } from '@vicons/carbon';
+  import AppLoginProviders from '@/components/Login/AppLoginProviders.vue';
 
-  const store = useStore();
   const router = useRouter();
-
   let loginError = ref<boolean>(false);
 
-  const authOptions: { provider: string; color: string } = [
-    { provider: 'Google', color: '#DB4437' },
-    { provider: 'Facebook', color: '#4267B2' },
-    { provider: 'Github', color: '#171515' },
-  ];
-
-  function signIn(provider: string) {
-    loginError.value = false;
-    store.dispatch('signIn', provider).catch(() => (loginError.value = true));
-  }
   function verifyToken() {
     if (localStorage.getItem('token')) {
       router.push({ name: 'Home' });
     }
   }
-
   verifyToken();
 </script>
 
@@ -73,27 +45,12 @@
       margin-bottom: 14px;
     }
 
-    &__button {
-      width: 100%;
-      padding: 24px 0;
-
-      &:not(&:nth-child(1)) {
-        margin-top: 14px;
-      }
-    }
-
     &__box {
       padding: 18px 14px;
       background: white;
       border-radius: 12px;
       box-shadow: 0px 0px 8px rgba($color: #000000, $alpha: 0.5);
       min-width: 280px;
-    }
-
-    span {
-      font-size: 16px;
-      font-weight: bold;
-      margin-left: 4px;
     }
   }
 </style>
